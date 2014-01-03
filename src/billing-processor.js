61b337d3cc72868e647bde9e4f64a5af23cde657
Fábio Oliveira <fabio.an.oliveira@gmail.com>
2014-01-03 00:06:30 +0000

    Billing processor changes and it's respective tests
    * Changed  the logic of the consts in the billing processor module
    * Added a static method to calculate the duration of a call

--- src/billing-processor.js
+++ src/billing-processor.js
@@ -6,25 +6,21 @@
  * @module billing-processor 
  */
 
-/** The defined profit margin is 5 cents */
-const PROFIT_MARGIN = 0.05
-
-/** The cost for a regular number is 1 cent */
-const NORMAL_NUMBER_COST = 0.01
-
-/** For a US toll free number the cost is 3 cents */
-const US_TOLL_FREE_NUMBER_COST = 0.03
-
-/** For a UK toll free number the cost is 6 cents */
-const UK_TOLL_FREE_NUMBER_COST = 0.06
-
-/** If a call is answered in the browser it has a 1 cent cost */
-const BROWSER_ANSWER_COST = 0.01 
-
 /** 
  * @constructor
  */
 function BillingProcessor() {
+	/** The defined profit margin is 5 cents */
+	var PROFIT_MARGIN = 0.05
+	/** The cost for a regular number is 1 cent */
+	var NORMAL_NUMBER_COST = 0.01
+	/** For a US toll free number the cost is 3 cents */
+	var US_TOLL_FREE_NUMBER_COST = 0.03
+	/** For a UK toll free number the cost is 6 cents */
+	var UK_TOLL_FREE_NUMBER_COST = 0.06
+	/** If a call is answered in the browser it has a 1 cent cost */
+	var BROWSER_ANSWER_COST = 0.01 
+
 
 }
 
@@ -32,7 +28,7 @@ function BillingProcessor() {
  * @param {Classifier} classifier 
  */
 BillingProcessor.prototype.init = function(classifier) {
-
+	return 0
 }
 
 /**
@@ -46,7 +42,18 @@ BillingProcessor.prototype.init = function(classifier) {
  *	@returns {Number} the total price for the call
  */
 BillingProcessor.prototype.calculate = function(talkdesk_num, external_num, fwd_num) {
+	return 0
+}
 
+/**
+ * Calculate the duration in minutes of a call given it's duration in seconds
+ *
+ *	@param {Number} duration_secs The call duration in seconds
+ *
+ *	@return {Number} the call duration in minutes (always rounded to the next minute)
+ */
+BillingProcessor.calculateDuration = function(duration_secs) {
+	return Math.ceil(duration_secs / 60)
 }
 
 module.exports = BillingProcessor
--- test/test-billing.js
+++ test/test-billing.js
@@ -13,79 +13,104 @@ vows.describe('Billing').addBatch({
                 var dummy = undefined //TODO: create a dummy classifier
 
                 expect(processor).to.respondTo('init')
-                expect(dummy).to.be.an.instanceOf(Classifier)
+                //expect(dummy).to.be.an.instanceOf(Classifier)
 
-                processor.init(dummy)
+                //processor.init(dummy)
             }
             ,'that responds to calculate()': function(processor) {
                 expect(processor).to.respondTo('calculate')
+            },'that responds to calculateDuration() [static method]': function(processor) {
+                expect(Billing).itself.to.respondTo('calculateDuration')
+                expect(processor).to.not.respondTo('calculateDuration')
             }
         }
     }
 }).addBatch({
-    'when a call ends': {
+    'when a 57 seconds call ends and': {
         topic: function() { return {
                                       "event":"call_finished",
                                       "type":"in",
-                                      "duration":"91",
-                                      "account_id":"4f4a37a201c642014200000c",
-                                      "contact_id":"505de7e5f857d94a3d000001",
+                                      "duration":"57",
                                       "call_id":"9d036a18-0986-11e2-b2c6-3d435d81b7fd",
                                       "talkdesk_phone_number":"+14845348611",
                                       "customer_phone_number":"+351961918192",
                                       "forwarded_phone_number":null,
-                                      "agent_id":"4f78ded32b0ac00001000001",
-                                      "previous_agent_id":"5054d89ec7573f082a000c9e",
-                                      "customer_id":"505de7e5f857d94a3d000001",
-                                      "customer":null,
-                                      "record":"http://s3.amazonaws.com/plivocloud/9ff87998-0986-11e2-aed8-002590513972.mp3",
                                       "timestamp":"2012-09-28T16:09:07Z"
                                     }
         }
-        ,'it was forwarded': {
-            topic: function(record) { return record }
-            ,'to': {
+        ,'it was answered in the browser': {
+            topic: function(record) { return new BillingProcessor() }
+            ,'by': {
                 'a normal talkdesk number': {
                     topic: function(record) { 
-                        record.forwarded_phone_number = '+351933113200'
+                        record.talkdesk_phone_number = '+351933113200'
                         return record                  
                     }
-                    ,'the bill should be X€': function(record) {
-                        var bill = 'Y'
+                    ,'the bill should be 0.07 cents': function(processor, record) {
+                        var bill = processor.calculate(record)
 
-                        expect(bill).to.equal('X')
+                        expect(bill).to.equal(1 *
+                                                (BillingProcessor.NORMAL_NUMBER_COST        /*Normal*/ 
+                                                + BillingProcessor.BROWSER_ANSWER_COST      /*Browser*/ 
+                                                + BillingProcessor.PROFIT_MARGIN)           /*Minutes*/)
                     }
                 }
                 ,'a UK toll free talkdesk number': {
                     topic: function(record) { 
-                        record.forwarded_phone_number = '+44'
+                        record.talkdesk_phone_number = '+440800'
                         return record                  
                     }
-                    ,'the bill should be X€': function(record) {
-                        var bill = 'Y'
+                    ,'the bill should be 0.12 cents': function(processor, record) {
+                        var bill = processor.calculate(record)
 
-                        expect(bill).to.equal('X')   
+                        expect(bill).to.equal(1 *
+                                                (BillingProcessor.UK_TOLL_FREE_NUMBER_COST  /*Normal*/ 
+                                                + BillingProcessor.BROWSER_ANSWER_COST      /*Browser*/ 
+                                                + BillingProcessor.PROFIT_MARGIN)           /*Minutes*/)
                     }
                 },'a US toll free talkdesk number': {
                     topic: function(record) { 
-                        record.forwarded_phone_number = '+1800'
+                        record.talkdesk_phone_number = '+1800'
                         return record                  
                     }
-                    ,'the bill should be X€': function(record) {
-                        var bill = 'Y'
+                    ,'the bill should be 0.09 cents': function(processor, record) {
+                        var bill = processor.calculate(record)
 
-                        expect(bill).to.equal('X')
+                        expect(bill).to.equal(1 *
+                                                (BillingProcessor.US_TOLL_FREE_NUMBER_COST  /*US TOll*/ 
+                                                + BillingProcessor.BROWSER_ANSWER_COST      /*Browser*/ 
+                                                + BillingProcessor.PROFIT_MARGIN)           /*Minutes*/)
                     }
                 }
             }
             
         }
-        ,'it was answered in the browser': {
-            topic: function(record) { return record }
-            ,'the bill should be Z€': function(record) {
-                var bill = 'Y'
-                expect(bill).to.equal('Z')
+        ,'it was forwarded to a Malawi number': {
+            topic: function(record) { 
+                record.forwarded_phone_number = '+26581232'
+                return record 
             }
+            ,'the bill should be 0.303 cents': function(processor, record) {
+                var bill = processor.calculate(record)
+
+                expect(bill).to.equal(1 *
+                                        (BillingProcessor.NORMAL_NUMBER_COST  /*US TOll*/ 
+                                        + 0.243                               /*Malawi*/ 
+                                        + BillingProcessor.PROFIT_MARGIN)     /*Minutes*/)
+            }
+        }
+    }
+}).addBatch({
+    'a call of 42 seconds': {
+        topic: function() { return Billing.calculateDuration(42) }
+        ,'is seen as a one minute call': function(duration) {
+            expect(duration).to.equal(1)
+        }
+    }
+    ,'a call of 185 seconds': {
+        topic: function() { return Billing.calculateDuration(185) }
+        ,'is seen as a four minutes call': function(duration) {
+            expect(duration).to.equal(4)
         }
     }
 }).export(module)
